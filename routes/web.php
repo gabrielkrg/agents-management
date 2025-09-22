47451dc6b4a048486e12be1fb38e0b572fc721a3
Gabriel <gabrielkrodrigues1@gmail.com>
2025-09-22 18:21:33 +0800

    minor

--- routes/web.php
+++ routes/web.php
@@ -48,4 +48,4 @@ Route::group(['middleware' => 'auth'], function () {
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
-require __DIR__ . '/api.php';
+// require __DIR__ . '/api.php';
