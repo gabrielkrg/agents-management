0e15a3cf1d9ebf9b9d8b6a7439d022a296ddda9e
Gabriel <gabrielkrodrigues1@gmail.com>
2025-11-11 16:34:33 +0800

    add prompts endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Controllers\Api\AiController;
+use App\Http\Controllers\PromptController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->group(function () {
+    //Prompt
+    Route::get('prompts', [PromptController::class, 'index'])->name('prompts.index');
+
+    //Ai
     Route::post('/ai/{prompt}', [AiController::class, 'index'])->name('api.ai');
     Route::post('/ai/generate-content/{prompt}', [AiController::class, 'generateContent'])->name('api.ai.generate-content');
 });
